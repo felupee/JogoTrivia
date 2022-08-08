fcb1be5895476062bc17262a66b2222cd012e962
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-08-08 17:33:02 -0300

    requisito 4 corrigido

--- src/tests/helpers/LoginPage.test.js
+++ src/tests/helpers/LoginPage.test.js
@@ -2,17 +2,18 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../../App';
-import renderWithRouter from './renderWithRouter';
+// import renderWithRouter from './renderWithRouter';
+import renderWithRouterAndRedux from './renderWithRouterAndRedux';
 
 describe('Testa pagina de login', () => {
     test('Testa se renderiza a rota correta', () => {
-        const { history } = renderWithRouter(<App />)
+        const { history } = renderWithRouterAndRedux(<App />)
         const { location: {pathname}  } = history;
         expect(pathname).toBe('/');
     })
 
     test('Verifica se os elementos foram renderizados corretamente', () => {
-        renderWithRouter(<App />);
+        renderWithRouterAndRedux(<App />);
         const nameField = screen.getByTestId('input-player-name');
         const emailField = screen.getByTestId('input-gravatar-email');
         const playButton = screen.getByTestId('btn-play')
@@ -25,7 +26,7 @@ describe('Testa pagina de login', () => {
     })
 
     test('Verifica se a pagina é redirecionada' , async() => {
-        const { history } = renderWithRouter(<App />);
+        const { history } = renderWithRouterAndRedux(<App />);
         const nameField = screen.getByTestId('input-player-name');
         const emailField = screen.getByTestId('input-gravatar-email');
         const playButton = screen.getByTestId('btn-play')
@@ -37,15 +38,15 @@ describe('Testa pagina de login', () => {
         expect(emailField.value).toBe('teste@teste.com')
         userEvent.click(playButton)
 
-        const gameTitle = await screen.findByText('Game');
+        const image = await screen.findByTestId('header-profile-picture');
         const { location: {pathname}  } = history;
         expect(pathname).toBe('/game'); 
-       expect(gameTitle).toBeInTheDocument(); 
+       expect(image).toBeInTheDocument(); 
        
     })
 
     test('Verifica se a pagina settings é redirecionada', ()=>{
-        const { history } = renderWithRouter(<App />);
+        const { history } = renderWithRouterAndRedux(<App />);
         const settingsButton = screen.getByTestId('btn-settings')
         userEvent.click(settingsButton)
         
