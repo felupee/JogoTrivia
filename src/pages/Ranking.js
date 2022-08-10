3bbc5b46f06d44d4d3861e25e23e59ffcf089485
Ranieri Fernandes <ranierirfm@hotmail.com>
2022-08-10 19:17:13 -0300

    requisito19

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,12 +1,21 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
 class Ranking extends React.Component {
   render() {
     const { history } = this.props;
+    const rankingArray = JSON.parse(localStorage.getItem('ranking'))
+      .sort((a, b) => b.score - a.score);
     return (
       <div>
         <tittle data-testid="ranking-title">ranking-title</tittle>
+        { rankingArray.map((player, index) => (
+          <div key={ index }>
+            <p data-testid={ `player-name-${index}` }>{player.name}</p>
+            <p data-testid={ `player-score-${index}` }>{player.score}</p>
+            <img src={ player.picture } alt="gravatar" />
+          </div>
+        ))}
         <button
           type="button"
           onClick={ () => history.push('/') }
