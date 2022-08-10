3381d1f7c4cf6b16aa7be7fee14ecdfc270e1e27
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-08-10 17:50:17 -0300

    requisito 18 testado

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
   render() {
+    const { history } = this.props;
     return (
-      <tittle data-testid="ranking-title">ranking-title</tittle>
+      <div>
+        <tittle data-testid="ranking-title">ranking-title</tittle>
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-go-home"
+        >
+          Home
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.objectOf(PropTypes.object).isRequired,
+};
+
 export default Ranking;
